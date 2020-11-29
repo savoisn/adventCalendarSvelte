b08862faa49c42a167d8c4884a4a88da7502646e
Nicolas Savois <nicolas.savois@gmail.com>
2020-11-29 08:16:53 +0100

    26 doors - need css

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let name;
+	import Door from './Door.svelte';
+
+	let calendarDays = [...Array(26).keys()];
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	{#each calendarDays as doorNumber}
+	<Door doorNumber = {doorNumber+1}/>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Door.svelte
@@ -0,0 +1,75 @@
+<script>
+  export let doorNumber=12;
+    function toggleDoor() {
+      doorOpen=!doorOpen
+    }
+    let doorOpen = false;
+</script>
+
+<main>
+    <div class="backDoor" >
+        <div class="backgroundPicture"></div>
+        <div class="door"     class:doorOpen={doorOpen} 
+        on:click={toggleDoor} 
+        >
+    <span class="doorNumber">{doorNumber}</span>
+        </div>
+    </div>
+</main>
+
+<style>
+
+:root {
+  --door-width: 100px;
+  --door-height: 150px;
+  --margin: 25px;
+  --margin-left:35px;
+  --door-number-fontsize:30px;
+}
+
+.backDoor {
+  background-color: #333;
+
+  position:relative;
+  width: var(--door-width);
+  height: var(--door-height);
+  margin: var(--margin);
+  margin-left:var(--margin-left);
+}
+
+.backgroundPicture {
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-image: url("https://www.xn--icne-wqa.com/images/icones/2/6/christmas-tree.png");
+  width: var(--door-width);
+  height: var(--door-height);
+  background-position: center;
+}
+
+.door {
+  background-color: brown;
+  position:absolute;
+  top:0px;
+  left:0px;
+  width:var(--door-width);
+  height:var(--door-height);
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+  transform-origin: left;
+  /*Speed of the Door animation*/
+  transition: all 0.5s ease-in-out;
+}
+
+.doorOpen {
+  /*prespectiv creates the door open effect*/
+  transform: perspective(1200px) translateZ(0px) translateX(0px) translateY(0px) rotateY(-105deg);
+}
+
+.doorNumber { 
+  line-height: var(--door-height);
+  vertical-align: middle;
+  font-size: var(--door-number-fontsize);
+}
+
+</style>
